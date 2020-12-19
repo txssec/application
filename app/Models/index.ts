5ef8228f47d271027a1426b025087d9715e31230
João Lenon <lenonsec7@gmail.com>
2020-12-19 18:18:05 -0300

    feat: Call Models in index

--- app/Models/index.ts
+++ app/Models/index.ts
@@ -1,4 +1,7 @@
 export * from './Role'
+export * from './Issue'
+export * from './Address'
+export * from './Contact'
 export * from './Permission'
 export * from './Attachment'
 export * from './Application'
