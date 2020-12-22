3fc02bddd44c5ba39cbf1f43e99db4780558ad7d
João Lenon <lenonsec7@gmail.com>
2020-12-22 09:43:42 -0300

    feat: Add new method for approved application only

--- app/Services/BaseService.ts
+++ app/Services/BaseService.ts
@@ -20,6 +20,28 @@ export class BaseService {
       throw new InternalServerException('APPLICATION_UNSETED')
     }
 
+    if (this._application.status === 'pendent_issue') {
+      return this._application
+    }
+
+    if (this._application.status !== 'approved') {
+      this.verifyStatus(this._application)
+    }
+
+    return this._application
+  }
+
+  public get application(): Application {
+    if (!this._guard) {
+      throw new InternalServerException('GUARD_UNSETED')
+    }
+
+    this._application = this._guard.user
+
+    if (!this._application) {
+      throw new InternalServerException('APPLICATION_UNSETED')
+    }
+
     if (this._application.status !== 'approved') {
       this.verifyStatus(this._application)
     }
@@ -42,6 +64,12 @@ export class BaseService {
   }
 
   public verifyStatus(application: Application) {
+    if (application.status === 'pendent_issue') {
+      throw new UnauthorizedException(
+        'Your profile is still pendent by some issue, please finalize your registration to proceed'
+      )
+    }
+
     if (application.status === 'pendent') {
       throw new UnauthorizedException(
         'Your profile is still pendent, please check your email inbox to approve it'
