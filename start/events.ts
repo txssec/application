61a1d38dbfc0771f62f4aae5ca6e27f6dac9f034
João Lenon <lenonsec7@gmail.com>
2020-12-22 09:44:34 -0300

    feat: Add new approve::application event

--- start/events.ts
+++ start/events.ts
@@ -1,5 +1,7 @@
 import Event from '@ioc:Adonis/Core/Event'
 
-Event.on('new::contact', 'IssueListener.resolveIssueContact')
-Event.on('new::address', 'IssueListener.resolveIssueAddress')
-Event.on('new::attachment', 'IssueListener.resolveIssueAttachment')
+Event.on('new::contact', 'IssueListener.resolveContact')
+Event.on('new::address', 'IssueListener.resolveAddress')
+Event.on('new::attachment', 'IssueListener.resolveAttachment')
+
+Event.on('approve::application', 'IssueListener.approveApplication')
